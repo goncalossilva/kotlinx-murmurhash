a683b6085be77a95e928c15c0b68da81bcf86b64
Gonçalo Silva <goncalossilva@gmail.com>
2021-12-09 04:13:02 +0000

    Port 32bit implementation to UInt
    As originally designed. Conversions to Int or Long are trivial.

--- src/commonMain/kotlin/MurmurHash3.kt
+++ src/commonMain/kotlin/MurmurHash3.kt
@@ -1,55 +1,65 @@
 package com.goncalossilva.murmurhash
 
-public class MurmurHash3(private val seed: Int = 0) {
-    public fun hash32x86(key: ByteArray): Int {
-        val len = key.size
+public class MurmurHash3(private val seed: UInt = 0u) {
+    public fun hash32x86(key: ByteArray): UInt {
         var h = seed
+        val len = key.size
+        val nblocks = len shr 2
 
-        var k: Int
-        for (i in 0 until (len ushr 2) * 4 step 4) {
-            k = key[i].toInt() or
-                (key[i + 1].toInt() shl 8) or
-                (key[i + 2].toInt() shl 16) or
-                (key[i + 3].toInt() shl 24)
+        for (i in 0 until nblocks) {
+            val index = i shl 2
+            val k = key.getUInt(index) or
+                (key.getUInt(index + 1) shl 8) or
+                (key.getUInt(index + 2) shl 16) or
+                (key.getUInt(index + 3) shl 24)
 
             h = h xor scramble(k)
-            h = (h shl R2) or (h ushr 32 - R2)
+            h = h.rotateLeft(R2)
             h = h * M + N
         }
 
-        k = 0
-        for (i in len % 4 downTo 1) {
-            k = k shl 8
-            k = k or key[len - i].toInt()
+        val index = nblocks shl 2
+        val rem = len - index
+        var k = 0u
+        if (rem == 3) {
+            k = k xor (key.getUInt(index + 2) shl 16)
         }
-        h = h xor scramble(k)
+        if (rem >= 2) {
+            k = k xor (key.getUInt(index + 1) shl 8)
+        }
+        if (rem >= 1) {
+            k = k xor key.getUInt(index)
+            h = h xor scramble(k)
+        }
 
-        h = h xor len
+        h = h xor len.toUInt()
 
-        h = h xor (h ushr 16)
-        h *= 0x85ebca6b.toInt()
-        h = h xor (h ushr 13)
-        h *= 0xc2b2ae35.toInt()
-        h = h xor (h ushr 16)
+        h = h xor (h shr 16)
+        h *= 0x85ebca6bu
+        h = h xor (h shr 13)
+        h *= 0xc2b2ae35u
+        h = h xor (h shr 16)
         return h
     }
 
-    private fun scramble(karg: Int): Int {
+    private fun scramble(karg: UInt): UInt {
         var k = karg
         k *= C1
-        k = (k shl R1) or (k ushr 32 - R1)
+        k = k.rotateLeft(R1)
         k *= C2
         return k
     }
 
+    private fun ByteArray.getUInt(index: Int) = get(index).toUInt()
+
     private companion object {
-        private const val C1: Int = 0xcc9e2d51.toInt()
-        private const val C2: Int = 0x1b873593
+        private const val C1: UInt = 0xcc9e2d51u
+        private const val C2: UInt = 0x1b873593u
 
         private const val R1: Int = 15
-        private const val R2: Int = 15
+        private const val R2: Int = 13
 
-        private const val M: Int = 5
-        private const val N: Int = 0xe6546b64.toInt()
+        private const val M: UInt = 5u
+        private const val N: UInt = 0xe6546b64u
     }
 }
--- src/commonTest/kotlin/MurmurHash3Test.kt
+++ src/commonTest/kotlin/MurmurHash3Test.kt
@@ -8,22 +8,22 @@ class MurmurHash3Test {
     private val words: List<String> by lazy {
         // English wordlist from sangupta/murmur:
         // https://github.com/sangupta/murmur/blob/592694f538a87d634418de5f849aed768a758147/src/test/resources/english/english-wordlist.txt
-        Resource("src/commonTest/resources/wordlist.txt").readText().lines()
+        Resource("src/commonTest/resources/wordlist.txt").readText().trim().lines()
     }
 
-    private val results32Bit: List<Int> by lazy {
+    private val results32Bit: List<UInt> by lazy {
         // Hashes computed by the canonical C++ implementation.
-        Resource("src/commonTest/resources/murmurhash3-32x86.txt").readText().lines().map {
-            it.toInt()
+        Resource("src/commonTest/resources/murmurhash3-32x86.txt").readText().trim().lines().map {
+            it.toUInt()
         }
     }
 
-    private val murmurHash3 = MurmurHash3(seed = 0x7f3a21ea)
+    private val murmurHash3 = MurmurHash3(seed = 0x7f3a21eau)
 
     @Test
     fun hash32Bit() {
         words.zip(results32Bit).forEach { (word, hash) ->
-            assertEquals(hash, murmurHash3.hash32x86(word.encodeToByteArray()))
+            assertEquals(hash, murmurHash3.hash32x86(word.encodeToByteArray()), word)
         }
     }
 }
